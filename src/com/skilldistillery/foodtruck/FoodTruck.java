2c5e86f76956cfc5a285543eb0d18a20b5482068
Brent Grauerholz <brentgrauerholz@yahoo.com>
2022-02-27 17:18:30 -0600

    first attempt

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,5 +1,60 @@
 package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
+	private int id;
+	private String truckName;
+	private String foodType;
+	private int rating;
+	private static int truckCounter = 1;
+	
+	public FoodTruck(int id, String tName, String fType, int rat) {
+	    id = truckCounter;
+	    truckCounter++;
+	    truckName = tName;
+	    foodType = fType;
+	    rating = rat;
+	  }
+	
+	
+	public FoodTruck() {
+		
+	}
+	@Override
+	public String toString() {
+		return "Truck ID-" + id + "\tname-" + truckName + "\tfood-" + foodType + "\trate-" + rating;
+	}
+
+	
+	public int getID() {
+		return id;
+	}
+	
+	public void setID(int id) {
+		this.id = id;
+	}
+	
+	public String getTruckName() {
+		return truckName;
+	}
+	
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+	public String getFoodType() {
+		return foodType;
+	}
+	
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+	public int getRating() {
+		return rating;
+	}
+	
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	
 
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -1,10 +1,60 @@
 package com.skilldistillery.foodtruck;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
 
+		FoodTruckApp fta = new FoodTruckApp();
+
+		FoodTruck[] trucks = new FoodTruck[5];
+
+				int id = 1;
+				String truckName = "";
+				String foodType = "";
+				int rating = 0;
+
+		//fta.list();
+
+		int i = 0;
+		while (i < 6) {
+			trucks[i] = new FoodTruck(id, truckName, foodType, rating);
+
+			System.out.println("Enter the name of a food truck or enter \"quit\" to end search. ");
+			trucks[i].setTruckName(input.nextLine());
+			if (trucks[i].getTruckName().equalsIgnoreCase("quit")) {
+				trucks[i] = null;
+				break;
+			}
+			System.out.println("Enter the type of food this truck serves: ");
+			trucks[i].setFoodType(input.nextLine());
+
+			while (trucks[i].getRating() > 5 || trucks[i].getRating() < 1) {
+				System.out.println("Rate this truck from 1 - 5");
+				trucks[i].setRating(input.nextInt());
+				input.nextLine();
+				break;
+
+			}
+			System.out.println("You have created food truck " + trucks[i].getTruckName() + ".");
+			i++;
+			
+		}
+			
 	}
-
 }
+	//System.out.println("ID Number: " + trucks[0].getID());
+
+	//}
+	//public void TrucksList(FoodTruck[] trucks) {
+		
+	//	int i = 0;
+	//	while(trucks[i] != null && i < 6) {
+	//		System.out.println(trucks[i].toString());
+	//	}
+		
+	
+
+//}
